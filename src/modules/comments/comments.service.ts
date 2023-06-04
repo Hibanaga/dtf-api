ec66159f2c4bb3acd3d3d64b674ee64706e979b4
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-06-04 18:44:17 +0200

    Minor fixes

--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Comment } from '../../models/Comment';
@@ -24,15 +24,23 @@ export class CommentsService {
 
   async single(id: string): Promise<Comment> {
     return await this.commentRepository.findOne({
-      where: {
-        id,
-      },
+      where: { id },
     });
   }
 
   async create(input: CreateCommentInput): Promise<Comment> {
     try {
-      return await this.single(input.id);
+      const isExistUser = await this.existsUser(input.userId);
+      if (!isExistUser) {
+        throw new HttpException('User not found!', HttpStatus.BAD_REQUEST);
+      }
+
+      const isExistPost = await this.existsPost(input.postId);
+      if (!isExistPost) {
+        throw new HttpException('Post not found!', HttpStatus.BAD_REQUEST);
+      }
+
+      return await this.commentRepository.save(input);
     } catch (e) {
       return e;
     }
@@ -40,9 +48,43 @@ export class CommentsService {
 
   async update(input: UpdateCommentInput) {
     try {
+      const isExistComment = await this.existsComment(input.id);
+      if (!isExistComment) {
+        throw new HttpException('Comment not found!', HttpStatus.BAD_REQUEST);
+      }
+
+      const isExistUser = await this.existsUser(input.userId);
+      if (!isExistUser) {
+        throw new HttpException('User not found!', HttpStatus.BAD_REQUEST);
+      }
+
+      const isExistPost = await this.existsPost(input.postId);
+      if (!isExistPost) {
+        throw new HttpException('Post not found!', HttpStatus.BAD_REQUEST);
+      }
+
+      await this.commentRepository.update(input.id, input);
       return await this.single(input.id);
     } catch (e) {
       return e;
     }
+  }
+
+  async existsUser(id: string) {
+    return this.userRepository.exist({
+      where: { id },
+    });
+  }
+
+  async existsPost(id: string) {
+    return this.postRepository.exist({
+      where: { id },
+    });
+  }
+
+  async existsComment(id: string) {
+    return this.commentRepository.exist({
+      where: { id },
+    });
   }
 }
